4dfc24a896a4925fa3b474f0fda0347922ffa2f7
Somar Melkhem <somar_melem@hotmail.com>
2018-07-19 17:41:56 +0300

    remove raw coverage and replace it with combiners services

---
+++ lib/simplecov/combiners/base_combiner.rb
@@ -0,0 +1,34 @@
+module SimpleCov
+  module Combiners
+    #
+    # Represents the base behavior or the combiner
+    # take two coverage statisitcs and combine them depends on the logic needed
+    #
+    class BaseCombiner
+      attr_reader :first_coverage, :second_coverage
+
+      def self.combine!(*args)
+        new(*args).combine
+      end
+
+      def initialize(first_coverage, second_coverage)
+        @first_coverage  = first_coverage
+        @second_coverage = second_coverage
+      end
+
+      def combine
+        puts "override this method"
+      end
+
+      private
+
+      def empty_coverage?
+        first_coverage && second_coverage
+      end
+
+      def existed_coverage
+        first_coverage || second_coverage
+      end
+    end
+  end
+end
--- spec/results_combiner_spec.rb
+++ spec/results_combiner_spec.rb
@@ -3,12 +3,16 @@
 require "helper"
 
 if SimpleCov.usable?
-  describe SimpleCov::RawCoverage do
+  describe SimpleCov::ResultsCombiner do
     describe "with two faked coverage resultsets" do
       before do
         @resultset1 = {
-          source_fixture("sample.rb") => {:lines => [nil, 1, 1, 1, nil, nil, 1, 1, nil, nil]},
-          source_fixture("app/models/user.rb") => {:lines => [nil, 1, 1, 1, nil, nil, 1, 0, nil, nil]},
+          source_fixture("sample.rb") => {
+            :lines => [nil, 1, 1, 1, nil, nil, 1, 1, nil, nil],
+            :branches =>{ [:if, 3, 8, 6, 8, 36] => { [:then, 4, 8, 6, 8, 12]=>47, [:else, 5, 8, 6, 8, 36]=>24}}
+          },
+          source_fixture("app/models/user.rb") => {:lines => [nil, 1, 1, 1, nil, nil, 1, 0, nil, nil],
+            :branches =>{ [:if, 3, 8, 6, 8, 36] => { [:then, 4, 8, 6, 8, 12]=>47, [:else, 5, 8, 6, 8, 36]=>24}}},
           source_fixture("app/controllers/sample_controller.rb") => {:lines => [nil, 1, 1, 1, nil, nil, 1, 0, nil, nil]},
           source_fixture("resultset1.rb") => {:lines => [1, 1, 1, 1]},
           source_fixture("parallel_tests.rb") => {:lines =>[nil, 0, nil, 0]},
@@ -18,7 +22,8 @@ if SimpleCov.usable?
 
         @resultset2 = {
           source_fixture("sample.rb") => {:lines => [1, nil, 1, 1, nil, nil, 1, 1, nil, nil]},
-          source_fixture("app/models/user.rb") => {:lines => [nil, 1, 5, 1, nil, nil, 1, 0, nil, nil]},
+          source_fixture("app/models/user.rb") => {:lines => [nil, 1, 5, 1, nil, nil, 1, 0, nil, nil],
+            :branches =>{ [:if, 3, 8, 6, 8, 36] => { [:then, 4, 8, 6, 8, 12]=>1, [:else, 5, 8, 6, 8, 36]=>2}}},
           source_fixture("app/controllers/sample_controller.rb") => {:lines => [nil, 3, 1, nil, nil, nil, 1, 0, nil, nil]},
           source_fixture("resultset2.rb") => {:lines => [nil, 1, 1, nil]},
           source_fixture("parallel_tests.rb") => {:lines => [nil, nil, 0, 0]},
@@ -33,15 +38,18 @@ if SimpleCov.usable?
 
       context "a merge" do
         subject do
-          SimpleCov::RawCoverage.merge_results(@resultset1, @resultset2, @resultset3)
+          SimpleCov::ResultsCombiner.combine!(@resultset1, @resultset2, @resultset3)
         end
 
         it "has proper results for sample.rb" do
           expect(subject[source_fixture("sample.rb")][:lines]).to eq([1, 1, 2, 2, nil, nil, 2, 2, nil, nil])
+          expect(subject[source_fixture("sample.rb")][:branches][[:if, 3, 8, 6, 8, 36]][[:then, 4, 8, 6, 8, 12]]).to eq(47)
         end
 
         it "has proper results for user.rb" do
           expect(subject[source_fixture("app/models/user.rb")][:lines]).to eq([nil, 2, 6, 2, nil, nil, 2, 0, nil, nil])
+          expect(subject[source_fixture("app/models/user.rb")][:branches][[:if, 3, 8, 6, 8, 36]][[:then, 4, 8, 6, 8, 12]]).to eq(48)
+          expect(subject[source_fixture("app/models/user.rb")][:branches][[:if, 3, 8, 6, 8, 36]][[:else, 5, 8, 6, 8, 36]]).to eq(26)
         end
 
         it "has proper results for sample_controller.rb" do
@@ -84,9 +92,10 @@ if SimpleCov.usable?
         source_fixture("sample.rb").freeze => {:lines => [1, nil, 1, 1, nil, nil, 1, 1, nil, nil]},
       }
 
-      merged_result = SimpleCov::RawCoverage.merge_results(resultset1, resultset2)
+      merged_result = SimpleCov::ResultsCombiner.combine!(resultset1, resultset2)
       expect(merged_result.keys).to eq(resultset1.keys)
       expect(merged_result.values.map(&:frozen?)).to eq([false, false])
+
       expect(merged_result[source_fixture("sample.rb")][:lines]).to eq([1, 1, 2, 2, nil, nil, 2, 2, nil, nil])
       expect(merged_result[source_fixture("app/models/user.rb")][:lines]).to eq([nil, 1, 1, 1, nil, nil, 1, 0, nil, nil])
     end
